457eb2c615bbf82bc59f9dde2075fddc54e6e5a4
Patiny-V0403 <phanthienvy2005@gmail.com>
2026-05-24 20:41:22 +0700

    create Content & Lesson for Project

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const userRoutes = require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoutes'); 
 const adminRoutes = require('./routes/adminRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
+const courseContentRoutes = require('./routes/courseContentRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -34,6 +35,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/categories', categoryRoutes);
+app.use('/api/content', courseContentRoutes);
 
 // Khởi động server
 app.listen(PORT, async () => {
